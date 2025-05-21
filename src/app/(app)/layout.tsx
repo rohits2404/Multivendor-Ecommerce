bc2b65b25762fe80505fcbd5e0781db1aa8ac492
Rohit Sharma <rohits8731@gmail.com>
2025-05-21 23:23:49 +0530

    first commit

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "@/components/ui/sonner";
+import { NuqsAdapter } from "nuqs/adapters/next/app"
 
 const dmSans = DM_Sans({
     subsets: ["latin"]
@@ -22,10 +23,12 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             <body
             className={`${dmSans.className} antialiased`}
             >
-                <TRPCReactProvider>
-                    {children}
-                    <Toaster/>
-                </TRPCReactProvider>
+                <NuqsAdapter>
+                    <TRPCReactProvider>
+                        {children}
+                        <Toaster/>
+                    </TRPCReactProvider>
+                </NuqsAdapter>
             </body>
         </html>
     );
--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -6,10 +6,18 @@ import { Category } from "@/payload-types";
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure
     .input(z.object({
-        category: z.string().nullable().optional()
+        category: z.string().nullable().optional(),
+        minPrice: z.string().nullable().optional(),
+        maxPrice: z.string().nullable().optional()
     }))
     .query(async ({ ctx, input }) => {
         const where: Where = {}
+        if(input.minPrice) {
+            where.price = { greater_than_equal: input.minPrice }
+        }
+        if(input.maxPrice) {
+            where.price = { less_than_equal: input.maxPrice }
+        }
         if(input.category) {
             const categoriesData = await ctx.db.find({
                 collection: "categories",
@@ -35,9 +43,9 @@ export const productsRouter = createTRPCRouter({
                 subCategoriesSlugs.push(
                     ...parentCategory.subcategories.map((subcategory)=>subcategory.slug)
                 )
-            }
-            where["category.slug"] = {
-                in: [parentCategory.slug, ...subCategoriesSlugs]
+                where["category.slug"] = {
+                    in: [parentCategory.slug, ...subCategoriesSlugs]
+                }
             }
         }
         const data = await ctx.db.find({
