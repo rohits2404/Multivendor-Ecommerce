d8fca1b3bc3489651d494582e1e56bd23e19cf18
Rohit Sharma <rohits8731@gmail.com>
2025-05-22 00:24:10 +0530

    final commit

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -11,12 +11,22 @@ export const productsRouter = createTRPCRouter({
         maxPrice: z.string().nullable().optional()
     }))
     .query(async ({ ctx, input }) => {
-        const where: Where = {}
-        if(input.minPrice) {
-            where.price = { greater_than_equal: input.minPrice }
+        const where: Where = {
+            price: {}
         }
-        if(input.maxPrice) {
-            where.price = { less_than_equal: input.maxPrice }
+        if(input.minPrice && input.maxPrice) {
+            where.price = { 
+                greater_than_equal: input.minPrice,
+                less_than_equal: input.maxPrice
+            }
+        } else if (input.minPrice) {
+            where.price = {
+                greater_than_equal: input.minPrice
+            }
+        } else if(input.maxPrice) {
+            where.price = {
+                less_than_equal: input.maxPrice
+            }
         }
         if(input.category) {
             const categoriesData = await ctx.db.find({
